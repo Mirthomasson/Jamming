0d0183cc20d65cadfb12104f1816f4280e3e2f29
MirThomasson <93678569+Mirthomasson@users.noreply.github.com>
2022-06-09 12:48:28 -0400

    Update App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,7 +5,6 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 import Spotify from '../../util/Spotify/Spotify';
 
-Spotify.getAccessToken();
 
 class App extends React.Component {
   constructor(props) {

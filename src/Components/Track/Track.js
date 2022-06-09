90c6cb0f35377ba92b8b550c23ba9d83129cb3a1
MirThomasson <93678569+Mirthomasson@users.noreply.github.com>
2022-06-09 12:35:06 -0400

    Update Track.js

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -7,18 +7,20 @@ class Track extends React.Component {
         this.addTrack = this.addTrack.bind(this);
         this.removeTrack = this.removeTrack.bind(this);
     }
+
+    addTrack() {
+        this.props.onAdd(this.props.track);
+    }
+    removeTrack() {
+        this.props.onRemove(this.props.track);
+    }
+
     renderAction() {
         if(this.props.isRemoval) {
             return <button className="Track-action" onClick={this.removeTrack}>-</button>;
         } else {
             return <button className="Track-action" onClick={this.addTrack}>+</button>
         }
-    }
-    addTrack() {
-        this.props.onAdd(this.props.track);
-    }
-    removeTrack() {
-        this.props.onRemove(this.props.track);
     }
     
     render() {
@@ -27,15 +29,10 @@ class Track extends React.Component {
                 <div className="Track-information">
                     <h3>{this.props.track.name}</h3>
                     <p>{this.props.track.artist} | {this.props.track.album}</p>
-                    <audio
-                    controls
-                    src = {this.props.track.preview}
-                    >
-                    </audio>
                 </div>
-                {this.renderAction()}
+                {this.renderAction(this.props.track)}
             </div>
-        )
+        );
     }
 }
 

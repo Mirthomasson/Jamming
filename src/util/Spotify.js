88c70fdd3b14b0900b9686317868705fe40a2002
MirThomasson <93678569+Mirthomasson@users.noreply.github.com>
2022-06-09 11:22:09 -0400

    Update Spotify.js

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -49,6 +49,10 @@ const Spotify = {
             }
             const accessToken = Spotify.getAccesToken();
             const headers = {Authorization: `Bearer ${accessToken}`};
+            const headersPOST = { 
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json' 
+            };
             let userId;
             return fetch('https://api.spotify.com/v1/me', {headers: headers}
             ).then(response => response.json()
@@ -56,7 +60,7 @@ const Spotify = {
                 userId = jsonResponse.id;
                 return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, 
                 {
-                    headers: {"content-type": "application/json"},
+                    headers: headersPOST,
                     method: 'POST',
                     body: JSON.stringify({name: name})
                 }).then(response => response.json()
@@ -64,7 +68,7 @@ const Spotify = {
                     const playlistId = jsonResponse.id;
                     return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, 
                 {
-                   headers: {"content-type": "application/json"},
+                   headers: headersPOST,
                    method: 'POST',
                    body:  JSON.stringify({uris: trackURIs})
                 });

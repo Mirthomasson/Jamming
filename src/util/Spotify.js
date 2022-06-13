2f4321cfdfb81e8fe65405b9284e2e77ac37c399
MirThomasson <93678569+Mirthomasson@users.noreply.github.com>
2022-06-13 12:10:35 -0400

    Update Spotify.js

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -20,9 +20,8 @@ const Spotify = {
             window.history.pushState('Access Token', null, '/');
             return accessToken;
         } else {
-            const accessUrl = `https://accounts.spotify.com/authorize?
-            client_id=${clientId}&response_type=token&
-            scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            const accessUrl = 
+            `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
             window.location = accessUrl;
          }
         },
